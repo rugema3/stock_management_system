366aab46fd1a9c2fd0dfe5dd38a12f988dac6ae5
rugema3 <rugema61@gmail.com>
2023-10-08 21:48:47 +0000

    pushing the recent updates

--- app.py
+++ app.py
@@ -1,13 +1,28 @@
 from flask import Flask, render_template, request
 from app.models.stock_manager import StockManager
 from app.models.stock_item import StockItem
+from app.models.database_manager import Database
+from decouple import config  
 
 app = Flask(__name__, template_folder='app/templates')
 stock_manager = StockManager()
 
+# Retrieve database configuration from environment variables
+db_config = {
+    'user': config('DB_USER'),
+    'password': config('DB_PASSWORD'),
+    'host': config('DB_HOST'),
+    'database': config('DB_NAME'),
+}
+
+# Create a Database instance
+db = Database(db_config)
+
 @app.route('/')
 def index():
-    return render_template('index.html', stock_items=stock_manager.items)
+    # Fetch stock items from the database using the Database class
+    stock_items = db.get_all_items()
+    return render_template('index.html', stock_items=stock_items)
 
 @app.route('/add_item', methods=['POST'])
 def add_item():
@@ -16,9 +31,17 @@ def add_item():
         price = float(request.form['price'])
         category = request.form['category']
         quantity = int(request.form['quantity'])
+
+        # Create a StockItem object with the UUID generated
         stock_item = StockItem(item_name, price, category, quantity)
-        stock_manager.add_item(stock_item)
-    return render_template('index.html', stock_items=stock_manager.items)
+
+        # Insert the new item into the database using the Database class
+        db.insert_item(stock_item)
+
+    # Fetch the updated stock items from the database
+    stock_items = db.get_all_items()
+
+    return render_template('index.html', stock_items=stock_items)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
--- app/models/database_manager.py
+++ app/models/database_manager.py
@@ -1,17 +1,106 @@
+"""
+Module: database_manager
+
+This module defines the Database class, which provides functionality for interacting
+with a MySQL database for a stock management system.
+"""
+
 import mysql.connector
 
-class DatabaseManager:
-    def __init__(self, host, user, password, database):
-        self.connection = mysql.connector.connect(
-            host=host,
-            user=user,
-            password=password,
-            database=database
-        )
-        self.cursor = self.connection.cursor()
+class Database:
+    """
+    A class for interacting with a MySQL database.
 
-    def insert_item(self, item_id, item_name, price, quantity, category, currency):
-        sql = "INSERT INTO stock_items (item_id, item_name, price, quantity, category, currency) VALUES (%s, %s, %s, %s, %s, %s)"
-        values = (item_id, item_name, price, quantity, category, currency)
-        self.cursor.execute(sql, values)
-        self.connection.commit()
+    Attributes:
+        db_connection: The MySQL database connection.
+    """
+
+    def __init__(self, db_config):
+        """
+        Initialize the Database class with the provided database configuration.
+
+        Args:
+            db_config (dict): A dictionary containing database connection parameters,
+                such as 'user', 'password', 'host', and 'database'.
+        """
+        self.db_connection = mysql.connector.connect(**db_config)
+
+    def insert_item(self, item):
+        """
+        Insert an item into the database.
+
+        Args:
+            item (StockItem): The StockItem object to be inserted into the database.
+        """
+        cursor = self.db_connection.cursor()
+        insert_query = "INSERT INTO stock_items(id, item_name, price, category, quantity) VALUES (%s, %s, %s, %s, %s)"
+        cursor.execute(insert_query, (item.item_id, item.item_name, item.price, item.category, item.quantity))
+        self.db_connection.commit()
+        cursor.close()
+
+    def get_all_items(self):
+        """
+        Retrieve all items from the database.
+
+        Returns:
+            list: A list of tuples containing the item records.
+        """
+        cursor = self.db_connection.cursor()
+        cursor.execute("SELECT * FROM stock_items")
+        items = cursor.fetchall()
+        cursor.close()
+        return items
+
+    def search_item(self, item_name):
+        """
+        Search for an item in the database by its name.
+
+        Args:
+            item_name (str): The name of the item to search for.
+
+        Returns:
+            list: A list of tuples containing the item records that match the search criteria.
+        """
+        cursor = self.db_connection.cursor()
+        search_query = "SELECT * FROM stock_items WHERE item_name = %s"
+        cursor.execute(search_query, (item_name,))
+        matching_items = cursor.fetchall()
+        cursor.close()
+        return matching_items
+
+    def checkout_item(self, item_name, quantity):
+        """
+        Checkout a specified quantity of an item from the stock.
+
+        Args:
+            item_name (str): The name of the item to be checked out.
+            quantity (int): The quantity of the item to be checked out.
+
+        Returns:
+            bool: True if the checkout was successful, False otherwise.
+        """
+        cursor = self.db_connection.cursor()
+        update_query = "UPDATE stock_items SET quantity = quantity - %s WHERE item_name = %s AND quantity >= %s"
+        cursor.execute(update_query, (quantity, item_name, quantity))
+        rows_affected = cursor.rowcount
+        self.db_connection.commit()
+        cursor.close()
+        return rows_affected > 0
+
+    def delete_item(self, item_name):
+        """
+        Delete an item from the database by its name.
+
+        Args:
+            item_name (str): The name of the item to be deleted.
+
+        Returns:
+            bool: True if the item was successfully deleted, False otherwise.
+        """
+        cursor = self.db_connection.cursor()
+        delete_query = "DELETE FROM stock_items WHERE item_name = %s"
+        cursor.execute(delete_query, (item_name,))
+        rows_affected = cursor.rowcount
+        self.db_connection.commit()
+        cursor.close()
+        return rows_affected > 0
